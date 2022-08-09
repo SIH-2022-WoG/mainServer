ac4da21d33f5d2a8e5bbc704ae9d72ca6080f477
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-08-09 20:48:31 +0530

    bug fix

--- server/professor/professorService.js
+++ server/professor/professorService.js
@@ -4,17 +4,20 @@ const Professor = require('./professorModel');
 const responseMessage = require('../utils/responseMessage');
 
 module.exports = {
-  updateProfile: (req, callback) => {
+  updateProfile: async (req, callback) => {
     let response;
     try {
       const userId = req.userId;
-      const prof = await Professor.findOneAndUpdate({user : userId}, req.body, 
-        {new : true, runValidators : true});
-      if( !prof ){
+      const prof = await Professor.findOneAndUpdate(
+        { user: userId },
+        req.body,
+        { new: true, runValidators: true }
+      );
+      if (!prof) {
         response = new responseMessage.ObjectDoesNotExistInDB();
-      }else{
-      response = new responseMessage.GenericSuccessMessage();
-      response.data = prof;
+      } else {
+        response = new responseMessage.GenericSuccessMessage();
+        response.data = prof;
       }
       return callback(null, response, response.code);
     } catch (err) {
