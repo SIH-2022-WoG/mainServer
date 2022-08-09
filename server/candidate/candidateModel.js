1d813718b450f0642d1db6a2cacc4a3fda61b1aa
Sayan Mondal <Sayan Mondal>
2022-08-09 13:32:22 +0530

    added schema

--- server/candidate/candidateModel.js
+++ server/candidate/candidateModel.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const candidateSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+    },
+    
+});
+
+candidateSchema.plugin(mongoosePaginate);
+module.exports = mongoose.mainConnection.model('Candidate',candidateSchema,'candidates');
+
--- server/college/collegeModel.js
+++ server/college/collegeModel.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const collegeSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+    },
+    
+});
+
+collegeSchema.plugin(mongoosePaginate);
+module.exports = mongoose.mainConnection.model('College',collegeSchema,'colleges');
+
