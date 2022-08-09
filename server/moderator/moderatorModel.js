cf7f898182f918fecef1666f47bfb3305a998b78
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-08-09 18:33:15 +0530

    moderatorModel

--- server/moderator/moderatorModel.js
+++ server/moderator/moderatorModel.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+const user = require('../user/userModel')
+
+const moderatorSchema = mongoose.Schema({
+    user : {
+        type : mongoose.Types.ObjectId,
+        ref : 'User',
+        unique : true
+    }
+});
+
+moderatorSchema.plugin(mongoosePaginate);
+module.exports = mongoose.mainConnection.model('moderator',moderatorSchema,'moderators');
