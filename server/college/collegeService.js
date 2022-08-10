3d61968519a9c3cf6143a5255abf79ad64d59536
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-08-10 20:23:39 +0530

    college creation done

--- server/college/collegeService.js
+++ server/college/collegeService.js
@@ -1,1 +1,20 @@
-'use strict'
+'use strict';
+
+const College = require('./collegeModel');
+const responseMessage = require('../utils/responseMessage');
+
+module.exports = {
+  createCollege: async (body, callback) => {
+    let response;
+    try {
+      const college = await College.create(body);
+      response = new responseMessage.GenericSuccessMessage();
+      response.data = college;
+      return callback(null, response, response.code);
+    } catch (err) {
+      console.log(err);
+      response = new responseMessage.GenericFailureMessage();
+      return callback(null, response, response.code);
+    }
+  },
+};
--- server/professor/professorController.js
+++ server/professor/professorController.js
@@ -2,10 +2,25 @@
 
 const professorService = require('./professorService');
 const responseHelper = require('../utils/responseHelper');
+const collegeService = require('../college/collegeService');
+const responseMessage = require('../utils/responseMessage');
 
 module.exports = {
   updateProfile: (req, res) =>
     professorService.updateProfile(req, (err, resdata, statuscode) => {
       responseHelper(err, res, resdata, statuscode);
     }),
+
+  createCollege: (req, res) => {
+    if (!req.user) {
+      const response = new responseMessage.GenericFailureMessage();
+      return responseHelper(null, res, response, response.code);
+    }
+
+    req.body.createdBy = req.user._id;
+
+    collegeService.createCollege(req.body, (err, resdata, statuscode) => {
+      responseHelper(err, res, resdata, statuscode);
+    });
+  },
 };
