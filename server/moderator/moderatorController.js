823f15ab34e5a7220bc32907e7054f9ff60497b5
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-08-13 21:07:35 +0530

    added routes

--- server/moderator/moderatorController.js
+++ server/moderator/moderatorController.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const collegeService = require('../college/collegeService');
+const studentService = require('../student/studentService');
+const professorService = require('../professor/professorService');
 const responseHelper = require('../utils/responseHelper');
 
 module.exports = {
@@ -11,7 +13,39 @@ module.exports = {
     });
   },
 
-  setStatus: (req, res) => {
+  getPendingStudents: (req, res) => {
+    req.searchQuery = { status: 'pending' };
+    collegeService.getColleges(req, (err, resdata, statuscode) => {
+      responseHelper(err, res, resdata, statuscode);
+    });
+  },
+
+  getPendingProfessors: (req, res) => {
+    req.searchQuery = { status: 'pending' };
+    collegeService.getColleges(req, (err, resdata, statuscode) => {
+      responseHelper(err, res, resdata, statuscode);
+    });
+  },
+
+  setStatusCollege: (req, res) => {
+    const user = req.user;
+    req.body.moderatedBy = user._id;
+
+    collegeService.updateProfile(req, (err, resdata, statuscode) => {
+      responseHelper(err, res, resdata, statuscode);
+    });
+  },
+
+  setStatusStudent: (req, res) => {
+    const user = req.user;
+    req.body.moderatedBy = user._id;
+
+    collegeService.updateProfile(req, (err, resdata, statuscode) => {
+      responseHelper(err, res, resdata, statuscode);
+    });
+  },
+
+  setStatusProfessor: (req, res) => {
     const user = req.user;
     req.body.moderatedBy = user._id;
 
