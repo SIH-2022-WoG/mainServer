0c4bf95c1e76f83b56c1df0ecb618b016c4d7138
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-08-26 05:45:45 +0530

    node fetch results

--- server/thesis/thesisController.js
+++ server/thesis/thesisController.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const fetch = require('node-fetch');
 const thesisService = require('./thesisService');
 const responseHelper = require('../utils/responseHelper');
+const responseMessage = require('../utils/responseMessage');
 
 module.exports = {
   getAllThesis: (req, res) => {
@@ -13,6 +15,42 @@ module.exports = {
   viewOne: (req, res) => {
     thesisService.viewOne(req, (err, resData, statusCode) => {
       return responseHelper(err, res, resData, statusCode);
+    });
+  },
+
+  evaluateThesis: (req, res) => {
+    let response;
+    thesisService.viewOne(req, async (err, resdata, statuscode) => {
+      if (parseInt(statuscode) === 200) {
+        const thesisData = resdata.data;
+        if (!thesisData.fulltext) {
+          response = new responseMessage.GenericFailureMessage();
+          return responseHelper(null, res, response, response.code);
+        }
+        try {
+          const lang = Number(req.query.lang) || 0;
+          const endPoint = ELASTIC_API + `/plagiarism/calculate/?lang=${lang}`;
+          const body = {
+            fileUrl: req.body.fileUrl,
+          };
+          const result = await fetch(endPoint, {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+          });
+          const data = await result.json();
+          // console.log(result);
+          response = new responseMessage.GenericSuccessMessage();
+          response.data = data;
+          return responseHelper(null, res, response, response.code);
+        } catch (err) {
+          console.log(err);
+          response = new responseMessage.GenericFailureMessage();
+          return responseHelper(null, res, response, response.code);
+        }
+      } else {
+        return responseHelper(err, res, resdata, statuscode);
+      }
     });
   },
 };
